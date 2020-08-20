cf779ab8a64a2c4799cfde6d904b3e7d908218dd
pankaj5395 <psharma5395@gmail.com>
2020-08-20 19:59:19 +0530

    add mongo db

--- server/config.js
+++ server/config.js
@@ -2,7 +2,8 @@ export default {
   // Secret key for JWT signing and encryption
   secret: 'a4f8071f-c873-4447-8ee2',
   // Database connection information
-  database: 'mongodb://localhost:27017',
+  //database: 'mongodb://localhost:27017',
+  database: 'mongodb://pankaj:test123@ds257495.mlab.com:57495/ravi_project',
   // Setting port for server
   port: 3000,
 };
