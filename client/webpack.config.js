bbfef036997cb9cf18c6c57fba6371a72949f4af
pankaj5395 <psharma5395@gmail.com>
2020-09-08 06:43:57 +0530

    webpack

--- client/webpack.config.js
+++ client/webpack.config.js
@@ -1,13 +1,45 @@
-module.exports = {
+const webpack = require('webpack');
+const path = require('path');
+const ExtractTextPlugin = require('extract-text-webpack-plugin');
+
+const config = {
+  context: __dirname,
+  entry: './src/index.js',
+  output: {
+    path: __dirname + '/dist',
+    filename: 'bundle.js',
+  },
   module: {
-    rules: [
+    loaders: [{
+      exclude: /node_modules/,
+      test: /\.(js|jsx)$/,
+      loader: 'babel',
+    },
       {
-        test: /\.(js|jsx)$/,
-        exclude: /node_modules/,
-        use: {
-          loader: "babel-loader"
-        }
-      }
-    ]
-  }
+        test: /\.scss$/,
+        loader: ExtractTextPlugin.extract('css!sass'),
+      }],
+  },
+  devServer: {
+    historyApiFallback: true,
+    contentBase: './',
+  },
+  plugins: [
+    new webpack.DefinePlugin({ 'process.env': { NODE_ENV: JSON.stringify('production') } }),
+    new webpack.optimize.DedupePlugin(),
+    new webpack.optimize.OccurenceOrderPlugin(),
+    new webpack.optimize.UglifyJsPlugin({
+      compress: { warnings: false },
+      output: { comments: false },
+      mangle: false,
+      sourcemap: false,
+      minimize: true,
+      mangle: { except: ['$super', '$', 'exports', 'require', '$q', '$ocLazyLoad'] },
+    }),
+    new ExtractTextPlugin('src/public/stylesheets/app.css', {
+      allChunks: true,
+    }),
+  ],
 };
+
+module.exports = config;
